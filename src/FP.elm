b74eb5a6b2a80a95133a49a4fb12e792ceae885e
Chris Keathley <c@keathley.io>
2016-08-04 23:23:26 -0400

    Week 1 Problem 1

--- src/FP.elm
+++ src/FP.elm
@@ -1,28 +1,60 @@
 module FP where
 
-import List   -- TODO: modify imports if you'd like
+import List
+import String
 
 digitsOfInt : Int -> List Int
 digitsOfInt n =
-  -- TODO
-  []
+  if n > 0 then
+    case n of
+      0 -> []
+      _ -> digitsOfInt (n // 10) ++ [n % 10]
 
-additivePersistence : Int -> Int
-additivePersistence n =
-  -- TODO
-  0
+  else
+    []
+
+sum : Int -> Int
+sum n =
+  n
+  |> digitsOfInt
+  |> List.sum
+
+genSequence : Int -> List Int
+genSequence n =
+  if n > 9 then
+    genSequence (sum n) ++ [n]
+
+  else
+    [n]
 
 digitalRoot : Int -> Int
 digitalRoot n =
-  -- TODO
-  0
+  let
+    seq =
+      n
+      |> genSequence
+  in
+    case List.head seq of
+      Just first -> first
+      Nothing -> 0
+
+additivePersistence : Int -> Int
+additivePersistence n =
+  List.length (genSequence n) - 1
 
 subsequences : List a -> List (List a)
 subsequences xs =
-  -- TODO
-  []
+  case xs of
+    [] -> [[]]
+    h::t -> subsequences t ++ List.map (\x -> h :: x) (subsequences t)
 
 take : Int -> List a -> Result String (List a)
 take k xs =
-  -- TODO
-  Err "..."
+  if List.length xs < k then
+    Err "not enough elements" 
+
+  else if k < 0 then
+    Err "negative index"
+
+  else
+    Ok (List.take k xs)
