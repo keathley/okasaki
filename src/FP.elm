aeb0506292687e42395a5e8e70bc24861f42bad9
Neil Menne <nhm@neilmenne.com>
2016-08-07 22:11:50 -0400

    HW1.1

--- src/FP.elm
+++ src/FP.elm
@@ -1,28 +1,47 @@
-module FP where
+module FP (digitsOfInt, additivePersistence, digitalRoot, subsequences, take) where
 
-import List   -- TODO: modify imports if you'd like
+import List exposing (sum, drop, map2, length, repeat)
 
 digitsOfInt : Int -> List Int
 digitsOfInt n =
-  -- TODO
-  []
+    if n < 0 then
+        []
+    else if n < 10 then
+        [n]
+    else
+        digitsOfInt(n//10) ++ [(n % 10)]
+
+
+digitalSeq : Int -> List Int
+digitalSeq n =
+    let n1 = n |> digitsOfInt |> sum in
+    if n1 < 10 then
+        [n1]
+    else
+        digitalSeq n1 ++ [n1]
 
 additivePersistence : Int -> Int
 additivePersistence n =
-  -- TODO
-  0
+    List.length(digitalSeq n)
 
 digitalRoot : Int -> Int
 digitalRoot n =
-  -- TODO
-  0
+    let drs = digitalSeq n in
+    case List.head drs of
+        Just h -> h
+        Nothing -> 0
 
 subsequences : List a -> List (List a)
 subsequences xs =
-  -- TODO
-  []
+  let l = length xs in
+  map2 drop [0..l] (repeat (l+1) xs)
 
 take : Int -> List a -> Result String (List a)
 take k xs =
-  -- TODO
-  Err "..."
+  let l = length xs in
+  if k < 0 then
+      Err "negative index"
+  else if k > l then
+      Err "not enough elements"
+  else
+      Ok (List.take k xs)
