898839eb7073396ec4fe0805ce919ecc2765452d
Josh Ziegler <space-pope@users.noreply.github.com>
2016-08-13 19:08:27 -0400

    Week 1

--- src/FP.elm
+++ src/FP.elm
@@ -1,28 +1,56 @@
-module FP where
+module FP (..) where
 
-import List   -- TODO: modify imports if you'd like
+import List
+
+digit : Int -> Int -> List Int
+digit n m =
+    case n of
+        0 -> []
+        _ ->
+          let s = n % m
+              d = s // (m // 10)
+          in
+          d :: (digit (n - s) (m * 10))
 
 digitsOfInt : Int -> List Int
 digitsOfInt n =
-  -- TODO
-  []
+    List.reverse <| digit n 10
+
+addAll : List Int -> Int -> (Int, Int)
+addAll l n =
+  let s = List.sum l
+  in
+    if s < 10 then
+      (s, n)
+    else
+      addAll (digitsOfInt s) (n + 1)
+
 
 additivePersistence : Int -> Int
 additivePersistence n =
-  -- TODO
-  0
+    snd <| addAll (digitsOfInt n) 1
+
 
 digitalRoot : Int -> Int
 digitalRoot n =
-  -- TODO
-  0
+    fst <| addAll (digitsOfInt n) 1
+
 
 subsequences : List a -> List (List a)
 subsequences xs =
-  -- TODO
-  []
+    case xs of
+      [] -> []
+      [x] -> [[x],[]]
+      x::ys ->
+        let zs = subsequences ys
+            addToAll = \x xs -> List.map ((::) x) xs
+        in addToAll x zs ++ zs
 
 take : Int -> List a -> Result String (List a)
 take k xs =
-  -- TODO
-  Err "..."
+  if k < 0 then
+    Err "negative index"
+  else if k > List.length xs then
+    Err "not enough elements"
+  else
+    Ok <| List.take k xs
