f2969cb00721cdef914f10a49a8b2df6f08670c6
Chris Keathley <c@keathley.io>
2016-08-15 11:18:29 -0400

    Finish problem 2

--- src/Pi.elm
+++ src/Pi.elm
@@ -1,85 +1,81 @@
 module Pi where
 
-import Html exposing (Html)
-import Html.App as App
-import Time exposing (Time, second) 
-import Collage as C 
-import Element as E
+import Random exposing (generate, map2, float)
+import Signal
+import Graphics.Element exposing (Element, empty)
+import Graphics.Collage as C exposing (Form)
+import Text
 import Color
---import Element exposing (Element, empty)
---import Element
-
---import String
---import Window
--- TODO: modify/add imports as needed
+import String
+import Window
+import Time exposing (millisecond)
 
 type alias Point = { x:Float, y:Float }
-type alias Model = ((Int, List Point), (Int, List Point))
-
-type Msg = Tick Time
-
-main =
-  App.program 
-  {
-    init = init
-  , view = view
-  , update = update
-  , subscriptions = subscriptions
-  }
-
-init : (Model, Cmd Msg)
-init =
-  (initState, Cmd.none)
+type alias State = ((Int, List Point), (Int, List Point))
 
 initState = ((0,[]), (0,[]))
 
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = 
-  case msg of
-    Tick newTime ->
-      (model, Cmd.none)
-
-upstate : Point -> Model -> Model
+upstate : Point -> State -> State
 upstate pt ((hc, hs), (mc, ms)) =
-  if pt.x^2 + pt.y^2 <= 1 then
-    ((hc+1, pt :: hs), (mc, ms))
-
-  else
-    ((hc, hs), (mc+1, pt :: ms))
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-  Time.every second Tick
-
-view : Model -> Html Msg
-view model =
   let
-    circ = C.filled Color.darkBlue (C.circle 20)
+    distance = sqrt (pt.x^2 + pt.y^2)
   in
-    C.collage 10 10 (circ)
+    if distance <= 1.0 then
+      ((hc+1, pt :: hs), (mc, ms))
 
+    else
+      ((hc, hs), (mc+1, pt :: ms))
 
---view : (Int,Int) -> State -> Element
---view (w,h) st =
---  -- TODO
---  empty
+view : (Int, Int) -> State -> Element
+view (w, h) ((hitCount, hs), (missCount, ms)) =
+  let
+    collageSize = toFloat h / 2 - 20
 
---genPoint : Random.Seed -> (Point, Random.Seed)
---genPoint s =
---  step (map2 Point (float 0 1) (float 0 1)) s
+    pointSize = 10
 
---signalPointSeed : Model -> 
---signalPointSeed =
---  Time.every second Tick
---  -- TODO
---  Signal.constant (genPoint (Random.initialSeed 0))
+    toHit point =
+      pointSize 
+      |> C.circle
+      |> C.filled Color.darkBlue
+      |> C.move (point.x * collageSize, point.y * collageSize)
 
---signalPoint : Signal Point
---signalPoint =
---  -- TODO
---  Signal.constant {x=0,y=0}
+    toMiss point =
+      pointSize
+      |> C.circle
+      |> C.filled Color.red
+      |> C.move (point.x * collageSize, point.y * collageSize)
 
---main : Signal Element
---  Signal.map2 view Window.dimensions
---  (Signal.foldp upstate initState signalPoint)
+    number =
+      (toFloat hitCount / toFloat (hitCount + missCount)) * 4.0
+      |> toString
+      |> Text.fromString
+      |> C.text
+      |> C.move (collageSize, collageSize)
+  in
+    C.collage w h ((List.map toHit hs) ++ (List.map toMiss ms) ++ [number])
+
+genPoint : Random.Seed -> (Point, Random.Seed)
+genPoint s =
+  let
+    gen = float -1 1
+  in
+    generate (map2 Point gen gen) s
+
+signalPointSeed : Signal (Point, Random.Seed)
+signalPointSeed =
+  let
+    initial = ({x=0, y=0}, Random.initialSeed 17)
+
+    next _ (_, seed) = genPoint seed
+  in
+    Signal.foldp next initial (Time.every 10)
+
+signalPoint : Signal Point
+signalPoint =
+  signalPointSeed
+  |> Signal.map fst
+
+main : Signal Element
+main =
+  Signal.map2 view Window.dimensions
+  (Signal.foldp upstate initState signalPoint)
