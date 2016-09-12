12a1f3029f1689e5a7b11f680f8bb7ca4f66d664
Stephen Spalding <sspalding@opentable.com>
2016-09-12 15:36:51 -0400

    Add hw1 solutions

--- src/FP.elm
+++ src/FP.elm
@@ -1,28 +1,32 @@
 module FP where
 
-import List   -- TODO: modify imports if you'd like
+import List exposing (sum, length, foldl, map)
 
 digitsOfInt : Int -> List Int
-digitsOfInt n =
-  -- TODO
-  []
+digitsOfInt n = if n < 10 then [n] else digitsOfInt (n // 10) ++ [n % 10]
+
+digitSums : Int -> List Int
+digitSums n = if n < 10 then [n] else n :: (digitsOfInt >> sum >> digitSums) n
 
 additivePersistence : Int -> Int
-additivePersistence n =
-  -- TODO
-  0
+additivePersistence = digitSums >> length >> (+) -1
+
+last = foldl (\x _ -> x) 0
 
 digitalRoot : Int -> Int
-digitalRoot n =
-  -- TODO
-  0
+digitalRoot = digitSums >> last
 
 subsequences : List a -> List (List a)
-subsequences xs =
-  -- TODO
-  []
+subsequences xs = case xs of
+  x::xs -> let xsSubs = subsequences xs in
+             map ((::) x) xsSubs ++ xsSubs
+  [] -> [[]]
 
 take : Int -> List a -> Result String (List a)
 take k xs =
-  -- TODO
-  Err "..."
+  if k < 0 then
+    Err "negative index"
+  else if k > length xs then
+    Err "not enough elements"
+  else
+    Ok (List.take k xs)
