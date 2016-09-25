806dc7b238cd2afb9d5f5d43169d6bde1af87483
Josh <nonvoulu@gmail.com>
2016-09-25 17:42:01 -0400

    exercise 3.1.1

--- src/LHeaps.elm
+++ src/LHeaps.elm
@@ -24,7 +24,7 @@ makeT x h1 h2 =
 merge : Heap -> Heap -> Heap
 merge h1 h2 = case (h1, h2) of
   (_, E) -> h1
-  (E, _) -> h2 
+  (E, _) -> h2
   (T _ x1 left1 right1, T _ x2 left2 right2) ->
     if x1 <= x2
       then makeT x1 left1 (merge right1 h2)
@@ -32,8 +32,26 @@ merge h1 h2 = case (h1, h2) of
 
 {------------------------------------------------------------------------}
 
-fromList : List Int -> Heap
-fromList _ =
-  -- TODO
-  E
+singleton : Int -> Heap
+singleton x = T 1 x E E
 
+-- I'm not sure how to form the recurrence relations mentioned in the
+-- assignment; what I do know is:
+-- . mergePairs and makePass both run in O(n/2) time (since they both merge
+--   elements two at a time), which reduces to O(n).
+
+fromList : List Int -> Heap
+fromList is = makePass <| mergePairs is
+
+mergePairs : List Int -> List Heap
+mergePairs is = case is of
+  [] -> []
+  [i] -> [singleton i]
+  (i1::i2::is) -> (merge (singleton i1) (singleton i2)) :: mergePairs is
+
+makePass : List Heap -> Heap
+makePass hs = case hs of
+  [] -> E
+  [h] -> h
+  [h1,h2] -> merge h1 h2
+  (h1::h2::hs) -> merge (merge h1 h2) (makePass hs)
