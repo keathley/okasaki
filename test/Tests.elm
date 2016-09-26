806ce50e6415271711d637162a2540bdb10c1c22
Stephen Spalding <sspalding@opentable.com>
2016-09-26 12:58:57 -0400

    HW 3.1.1 tests

--- test/Tests.elm
+++ test/Tests.elm
@@ -4,11 +4,16 @@ import ElmTest exposing (..)
 
 import FPTest
 import ListsAndTreesTest
+import LHeapsTest
 
 all : Test
 all =
     suite "Homework Problems"
         [
+         -- HW 1
             FPTest.tests,
-            ListsAndTreesTest.tests
+         -- HW 2
+            ListsAndTreesTest.tests,
+         -- HW 3
+            LHeapsTest.tests
         ]
