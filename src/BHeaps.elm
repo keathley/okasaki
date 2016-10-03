f54d4896a0632cb4e4bebf0240f9166fb2334793
Stephen Spalding <sspalding@opentable.com>
2016-10-02 20:32:35 -0400

    Hw 3.2.1 Solution

--- src/BHeaps.elm
+++ src/BHeaps.elm
@@ -8,7 +8,40 @@ type Heap = WrapHeap InternalHeap
 
 {-- Internal Helpers ----------------------------------------------------}
 
--- TODO
+rank (r, _) = r
+root (_, (Node x _)) = x
+
+link : (Rank, Tree) -> (Rank, Tree) -> (Rank, Tree)
+link (r, t1) (_, t2) = case (t1, t2) of
+    (Node x1 c1, Node x2 c2) ->
+        (r + 1, if x1 <= x2 then Node x1 (t2 :: c1)
+                else Node x2 (t1 :: c2))
+
+insertTree : (Rank, Tree) -> InternalHeap -> InternalHeap
+insertTree (r, t) ts = case ts of
+  []              -> [(r, t)]
+  (r', t') :: ts' ->
+      if r == r' then insertTree (link (r, t) (r', t')) ts'
+      else if r < r' then (r, t) :: (r', t') :: ts'
+      else Debug.crash "insertTree: impossible"
+
+
+merge_ : InternalHeap -> InternalHeap -> InternalHeap
+merge_ ts1 ts2 = case (ts1, ts2) of
+  ([], _) -> ts2
+  (_, []) -> ts1
+  (t1::ts1', t2::ts2') ->
+  if rank t1 < rank t2 then t1 :: merge_ ts1' ts2
+  else if rank t2 < rank t1 then t2 :: merge_ ts2' ts1
+  else insertTree (link t1 t2) (merge_ ts1' ts2')
+
+removeMinTree : InternalHeap -> ((Rank, Tree), InternalHeap)
+removeMinTree ts = case ts of
+  []     -> Debug.crash "removeMinTree: Impossible"
+  [t]    -> (t, [])
+  t::ts' -> let (t', ts'') = removeMinTree ts' in
+            if root t < root t' then (t, ts')
+            else (t', t::ts'')
 
 {-- External Interface --------------------------------------------------}
 
@@ -19,22 +52,19 @@ isEmpty : Heap -> Bool
 isEmpty h = h == empty
 
 insert : Int -> Heap -> Heap
-insert x h =
-  -- TODO
-  h
+insert x (WrapHeap ts) = WrapHeap <| insertTree (0, Node x []) ts
 
 merge : Heap -> Heap -> Heap
-merge h1 h2 =
-  -- TODO
-  h1
+merge (WrapHeap ts1) (WrapHeap ts2) = WrapHeap (merge_ ts1 ts2)
 
 findMin : Heap -> Maybe Int
-findMin h =
-  -- TODO
-  Nothing
+findMin (WrapHeap ts) = case List.map root ts of
+  [] -> Nothing
+  n :: ns -> Just <| List.foldl min n ns
 
 deleteMin : Heap -> Maybe Heap
-deleteMin h =
-  -- TODO
-  Nothing
-
+deleteMin (WrapHeap ts) = case ts of
+  [] -> Nothing
+  _  -> let ((r, Node x ts1), ts2) = removeMinTree ts in
+        let ts1' = (List.map (\t -> (r-1, t)) (List.reverse ts1)) in
+        Just (WrapHeap (merge_ ts1' ts2))
