e8ec6e83d8be56e7bfd416bf819092aade7a8aaf
Stephen Spalding <sspalding@opentable.com>
2016-10-02 21:17:11 -0400

    HW 3.2.2 Solution

--- src/ExplicitMin.elm
+++ src/ExplicitMin.elm
@@ -15,27 +15,37 @@ empty : Heap
 empty = E
 
 isEmpty : Heap -> Bool
-isEmpty _ =
-  -- TODO
-  False
+isEmpty h = h == empty
 
 insert : Int -> Heap -> Heap
-insert _ _ =
-  -- TODO
-  E
+insert x h = case h of
+  E -> NE x <| H.insert x H.empty
+  NE x' h' -> NE (min x x') <| H.insert x h'
 
 merge : Heap -> Heap -> Heap
-merge _ _ =
-  -- TODO
-  E
+merge h1 h2 = case (h1, h2) of
+  (_, E) -> h1
+  (E, _) -> h2
+  (NE x1 h1', NE x2 h2') -> NE (min x1 x2) <| H.merge h1' h2'
 
 findMin : Heap -> Maybe Int
-findMin _ =
-  -- TODO
-  Nothing
+findMin h = case h of
+  E -> Nothing
+  NE x _ -> Just x
 
 deleteMin : Heap -> Maybe Heap
-deleteMin _ =
-  -- TODO
-  Nothing
+deleteMin h = case h of
+  E -> Nothing
+  NE x h' -> let h'' = Maybe.withDefault H.empty <| H.deleteMin h' in
+                case H.findMin h'' of
+                    Nothing -> Nothing
+                    Just x' -> Just <| NE x' h''
 
+  -- Alternate implementation (without withoutDefault)
+
+  -- NE x h' ->
+  --     case H.deleteMin h' of
+  --         Nothing -> Nothing
+  --         Just h'' -> case H.findMin h'' of
+  --             Nothing -> Nothing
+  --             Just x' -> Just <| NE x' h''
